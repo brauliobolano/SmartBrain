a4056879c33c779423fba913f6a726e9fac7abe6
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-03-10 14:47:44 -0400

    first commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,9 @@
+const Navigation = () => {
+    return (
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='f3 link dim black underline pointer pa3'>Sign Out</p>
+        </nav>
+    );
+}
+
+export default Navigation;
