cb7612f47a2139c2aa8061b22273fa2143613537
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-03-10 15:35:28 -0400

    Added navigation, logo, and image link form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
   return (
     <div>
       <Navigation/>
-{/*       
       <Logo/>
       <ImageLinkForm/>
+{/*       
+      <Rank/>      
       <FaceRecognition/> */}
     </div>
   );
