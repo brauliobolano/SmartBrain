0ebde65fe4cc8229a1391e5c64aeb97ffd8ff1fb
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-05-06 20:51:10 -0400

    Connected front end and back end

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,31 @@
-const Navigation = () => {
-    return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end', position: "relative" }}>
-            {/* <p className='f3 link dim black underline pointer pa3'>Sign Out</p> */}
-        </nav>
-    );
+const Navigation = ({onRouteChange, isSignedIn}) => {
+    if(isSignedIn) {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end', position: "relative" }}>
+                <p 
+                    onClick={() => onRouteChange('signout')}
+                    className='f3 link dim black underline pointer pa3'>
+                    Sign Out
+                </p>
+            </nav>
+        );
+    } else {
+        return (
+            <nav style={{display: 'flex', justifyContent: 'flex-end', position: "relative" }}>
+                <p 
+                    onClick={() => onRouteChange('signin')}
+                    className='f3 link dim black underline pointer pa3'>
+                    Sign In
+                </p>
+                <p 
+                    onClick={() => onRouteChange('register')}
+                    className='f3 link dim black underline pointer pa3'>
+                    Register
+                </p>
+            </nav>
+        );
+    
+    }
 }
 
 export default Navigation;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   const [signInEmail, setSignInEmail] = useState("");
   const [signInPassword, setSignInPassword] = useState("");
+  const [signInName, setSignInName] = useState("");
 
   const onEmailChange = (event) => {
     setSignInEmail(event.target.value);
@@ -13,29 +13,45 @@ const SignIn = ({ onRouteChange }) => {
     setSignInPassword(event.target.value);
   };
 
-  const onSubmitSignIn = () => {
-    fetch("http://localhost:4000/signin", {
+  const onNameChange = (event) => {
+    setSignInName(event.target.value);
+  };
+
+  const onSubmitRegister = () => {
+    fetch("http://localhost:4000/register", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         email: signInEmail,
         password: signInPassword,
+        name: signInName,
       }),
     })
       .then((response) => response.json())
       .then((user) => {
-        if (user.id) {
+        if (user /* .id */) {
           onRouteChange("home");
         }
       });
   };
-
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
-        <form className="measure ">
+        <div className="measure ">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0 ">
-            <legend className="f1 fw6 ph0 mh0 center">Sign In</legend>
+            <legend className="f1 fw6 ph0 mh0 center">Register</legend>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6 center" htmlFor="name">
+                Name
+              </label>
+              <input
+                onChange={onNameChange}
+                className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                type="text"
+                name="name"
+                id="name"
+              />
+            </div>
             <div className="mt3">
               <label
                 className="db fw6 lh-copy f6 center"
@@ -66,26 +82,16 @@ const SignIn = ({ onRouteChange }) => {
           </fieldset>
           <div className="center">
             <input
-              /*  */
-              onClick={() => onSubmitSignIn()}
+              onClick={() => onSubmitRegister()}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Sign in"
             />
           </div>
-          <div className="lh-copy mt3 center">
-            <p
-              onClick={() => onRouteChange("register")}
-              href="#0"
-              className="f6 link dim black db pointer"
-            >
-              Register
-            </p>
-          </div>
-        </form>
+        </div>
       </main>
     </article>
   );
 };
 
-export default SignIn;
+export default Register;
